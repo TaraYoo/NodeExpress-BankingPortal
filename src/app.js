1c4e64f2be703c8132d87d55606ce8b29f2d929e
Tara Yoo <yootar@gmail.com>
2020-03-02 10:44:38 -0500

    Complete module 2

--- src/app.js
+++ src/app.js
@@ -9,9 +9,43 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
+const accountData = fs.readFileSync(path.resolve(__dirname, 'json/accounts.json'), 'utf8')
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync(path.resolve(__dirname, 'json/users.json'), 'utf8')
+const users = JSON.parse(userData)
+
+
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
+    })
+})
+
+app.get('/profile', function(req, res) {
+    res.render('profile', {
+        user: users[0]
+    })
+})
+
+
+app.get('/savings', function(req, res) {
+    res.render('account', {
+        account: accounts.savings
+    })
+})
+
+app.get('/checking', function(req, res) {
+    res.render('account', {
+        account: accounts.checking
+    })
+})
+
+    
+app.get('/credit', function(req, res) {
+    res.render('account', {
+        account: accounts.credit
     })
 })
 
