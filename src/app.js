dedcd2ee9b0647530c93d56a686bc74f413912d8
Tara Yoo <yootar@gmail.com>
2020-03-19 10:31:06 -0400

    Failing at compiling transfer.ejs

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded(extended=true))
 
 const accountData = fs.readFileSync(path.resolve(__dirname, 'json/accounts.json'), 'utf8')
 const accounts = JSON.parse(accountData)
@@ -47,6 +48,45 @@ app.get('/credit', function(req, res) {
     res.render('account', {
         account: accounts.credit
     })
+})
+
+app.get('/transfer', function(req, res) {
+    res.render('transfer')
+})
+
+app.post('/transfer', function(req, res) {
+    const from = req.body.from
+    const to = req.body.to
+    const amount = req.body.amount
+
+    accounts[from].balance -= parseInt(amount)
+    accounts[to].balance += parseInt(amount)
+
+    accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.resolve(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+    res.render('transfer', {
+        message: 'Transfer Completed'
+    })
+})
+
+app.get('/payment', function(req, res) {
+    res.render('payment', {
+        account: accounts.credit
+    })
+})
+
+app.post('/payment', function(req, res) {
+    accounts.credit.balance -= parseInt(req.body.amount)
+    accounts.credit.available += parseInt(req.body.amount)
+
+    accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.resolve(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+
+    res.render('payment', {
+        message: 'Payment Successful',
+        account: accounts.credit
+    })
+
 })
 
 app.listen(3000, function() {
